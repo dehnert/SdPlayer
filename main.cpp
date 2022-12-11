425447ba7d30e35f50a83c3298c688afe0170dc1
Alex Dehnert <adehnert@mit.edu>
2022-12-10 20:51:05 -0500

    use bundled plugins

--- main.cpp
+++ main.cpp
@@ -12,7 +12,8 @@ int main(int argc, char *argv[])
     setenv("VLC_PLUGIN_PATH", "/Applications/VLC.app/Contents/MacOS/plugins", 1);
 #endif
 #if defined(_WIN64)
-    SetEnvironmentVariable(TEXT("VLC_PLUGIN_PATH"), TEXT("C:/Program Files/VideoLAN/VLC"));
+    //SetEnvironmentVariable(TEXT("VLC_PLUGIN_PATH"), TEXT("C:/Program Files/VideoLAN/VLC"));
+    SetEnvironmentVariable(TEXT("VLC_PLUGIN_PATH"), TEXT("C:/Program Files/SdPlayer"));
 #endif
     QApplication a(argc, argv);
     a.setApplicationName("sdplayer");
